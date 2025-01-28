78f6b3ef7cbc31ecdbbac0dd9fc895bf0113fc35
mvindasr <mvindasr@ucenfotec.ac.cr>
2025-01-28 10:12:11 -0600

    add user, session and items redis queries

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,18 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: '1950'
+	});
+
+	const car = await client.hGetAll('car#34443434434');
+
+	if (Object.keys(car).length === 0) {
+		console.log('Car not found, respond with 404');
+		return;
+	}
+	console.log(car);
+};
 run();
